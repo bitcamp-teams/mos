cef59823cc958a05814708cce6fe52a4bbfed803
sunjong0214 <rlatjswhd8200@gmail.com>
2024-04-04 12:19:29 +0900

    github login refactoring

--- mos/src/main/java/com/mos/domain/member/service/MemberService.java
+++ mos/src/main/java/com/mos/domain/member/service/MemberService.java
@@ -5,6 +5,7 @@ import com.mos.domain.member.dto.MemberDto;
 public interface MemberService {
 
   MemberDto get(String email);
+
 //  int add(MemberJoinDto joinDto);
 
 }
--- mos/src/main/java/com/mos/domain/member/service/impl/GithubOAuthService.java
+++ mos/src/main/java/com/mos/domain/member/service/impl/GithubOAuthService.java
@@ -2,6 +2,7 @@ package com.mos.domain.member.service.impl;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -10,6 +11,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -18,25 +21,29 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
+//@ConfigurationProperties(prefix = "github")
+//@ConstructorBinding
 public class GithubOAuthService {
 
   private static final Log log = LogFactory.getLog(GithubOAuthService.class);
-  @Value("${spring.security.oauth2.client.registration.github.clientId}")
-  private String clientId;
-  @Value("${spring.security.oauth2.client.registration.github.clientSecret}")
-  private String clientSecret;
+
+  private final String clientId;
+
+  private final String clientSecret;
+
+  public GithubOAuthService(
+      @Value("${github.clientId}")String clientId,
+      @Value("${github.clientSecret}")String clientSecret) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+  }
 
   @Autowired
   private WebClient webClient;
 
-  public String getAccessToken(String code) throws IOException{
-    MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-    formData.add("client_id", clientId);
-    formData.add("client_secret", clientSecret);
-    formData.add("code", code);
-
-    String accessToken= webClient.post()
+  private String requestAccessToken(MultiValueMap<String, String> formData) {
+    return webClient.post()
         .uri("https://github.com/login/oauth/access_token")
         .header("Accept", "application/json")
         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
@@ -44,19 +51,32 @@ public class GithubOAuthService {
         .retrieve()
         .bodyToMono(String.class)
         .block();
-
-    String tokenParser = "{\"access_token\":\"";
-    if (accessToken == null) {
-      log.debug(String.format("accessToken 가져오기 실패"));
-      return null;
-    }
-    String resultToken = parsingResponse(accessToken, ",", tokenParser);
-    log.debug(String.format("Github accessToken = " + accessToken));
-    return getEmail(resultToken);
   }
 
-  public String parsingResponse(String str, String split, String parser)throws IOException {
-    String[] tokens = str.split(split);
+
+  public Optional<String> getAccessToken(String code){
+    MultiValueMap<String, String> formData = setFormData(code);
+
+    String tokenBeforeParsing = requestAccessToken(formData);
+
+    String tokenParser = "{\"access_token\":\"";
+    String tokenAfterParsing = parsingResponse(tokenBeforeParsing, tokenParser);
+    log.debug(String.format("Github accessToken = " + tokenAfterParsing));
+
+    return Optional.ofNullable(getEmail(tokenAfterParsing)).orElse(null);
+  }
+
+  private MultiValueMap<String, String> setFormData(String code) {
+    MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+    formData.add("client_id", clientId);
+    formData.add("client_secret", clientSecret);
+    formData.add("code", code);
+    return formData;
+  }
+
+
+  private String parsingResponse(String str, String parser) {
+    String[] tokens = str.split(",");
     //반복문 사용
     //for (String part : tokens) {
     //  System.out.println("part =" + part);
@@ -73,21 +93,24 @@ public class GithubOAuthService {
         .orElse(null);               // 없으면 null
   }
 
-  public String getEmail(String token) throws IOException{
-
-    OkHttpClient client = new OkHttpClient();
-
-    Request request = new Request.Builder()
-        .url("https://api.github.com/user/emails")
+  private String requestUserEmail(String token) {
+    return webClient.get()
+        .uri("https://api.github.com/user/emails")
         .header("Authorization", "token " + token)
-        .build();
+        .retrieve()
+        .bodyToMono(String.class)
+        .block();
+  }
 
-    try (Response response = client.newCall(request).execute()) {
-        String responseBody = response.body().string();
-        String emailParser = "[{\"email\":\"";
-        String email =  parsingResponse(responseBody, ",", emailParser);
-        log.debug(String.format("email = " + email));
-        return email;
+  public Optional<String> getEmail(String token) {
+
+    String emailBeforeParsing = requestUserEmail(token);
+
+    String emailParser = "[{\"email\":\"";
+    if (emailBeforeParsing == null) {
+      return Optional.empty();
     }
+    String emailAfterParsing = parsingResponse(emailBeforeParsing, emailParser);
+    return Optional.ofNullable(emailAfterParsing);
   }
 }
