d0b779d90a2d5eec814e346d13d9d3fa59b74220
Joonhoe Kim <26rote@gmail.com>
2024-04-08 16:14:56 +0900

    feat(wiki): wiki list 조회 기능 추가
    확인: 스터디 목록 -> 스터디 모집글 조회 -> 이 스터디의 위키 보러가기
    컨트롤러 매핑: /wiki/list?studyNo=

--- mos/src/main/java/com/mos/domain/wiki/service/impl/DefaultWikiService.java
+++ mos/src/main/java/com/mos/domain/wiki/service/impl/DefaultWikiService.java
@@ -10,19 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class DefaultWikiService implements WikiService {
+
   private final WikiRepository wikiRepository;
 
   @Override
-  public void add(WikiDto studyDto) {
-    wikiRepository.add(studyDto);
-  }
-
-  @Override
-  public WikiDto getByStudyNo(int studyNo) {
-    return wikiRepository.getByStudyNo(studyNo);
-  }
-  @Override
-  public List<WikiDto> list() {
-    return wikiRepository.findAll();
+  public List<WikiDto> listByStudyNo(int studyNo) {
+    return wikiRepository.listByStudyNo(studyNo);
   }
 }
