cbade48c238fe73049601cfa1e7d6b73ab8e8709
yusoonsun <tnstjs39@naver.com>
2024-04-16 18:49:57 +0900

    Feat(Study):Tag 입력,조회 Study/form 에서 Tag 입력 가능하게 함 Study/list 에 Tag 나오게 함

--- mos/src/main/java/com/mos/domain/study/dto/StudyDto.java
+++ mos/src/main/java/com/mos/domain/study/dto/StudyDto.java
@@ -1,6 +1,8 @@
 package com.mos.domain.study.dto;
 
 import java.sql.Date;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,18 +14,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudyDto {
-  int studyNo;
-  int memberNo;
-  String title;
-  String introduction;
-  String location;
-  Date startDate;
-  Date endDate;
-  String stat;
-  int intake;
-  String method;
-  Date recruitmentDeadline;
-  Date createdDate;
-  Date updatedDate;
-  int siggNo;
+  private int studyNo;
+  private int memberNo;
+  private String title;
+  private String introduction;
+  private String location;
+  private Date startDate;
+  private Date endDate;
+  private String stat;
+  private int intake;
+  private String method;
+  private Date recruitmentDeadline;
+  private Date createdDate;
+  private Date updatedDate;
+  private int siggNo;
+  private List<TagDto> tagList;
 }
