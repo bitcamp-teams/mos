653333d3d9475afe60573e88b0d498751cafce0d
sunjong0214 <rlatjswhd8200@gmail.com>
2024-04-17 18:07:34 +0900

    알림 목록 추가

--- mos/src/main/java/com/mos/domain/noti/service/NotiService.java
+++ mos/src/main/java/com/mos/domain/noti/service/NotiService.java
@@ -1,7 +1,11 @@
 package com.mos.domain.noti.service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mos.domain.noti.dto.NotiAddDto;
+import com.mos.domain.noti.dto.NotiDto;
 import com.mos.domain.noti.repository.NotiRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +15,19 @@ public class NotiService {
 
   private final NotiRepository notiRepository;
 
-  public void sendNoti(NotiAddDto notiDto) {
+  public void add(NotiAddDto notiDto) {
     System.out.println("notiDto = " + notiDto);
     notiRepository.add(notiDto);
+  }
+
+  public String list(int id) {
+    List<NotiDto> list = notiRepository.findById(id);
+
+    Gson gson = new GsonBuilder()
+        .setPrettyPrinting()
+        .disableHtmlEscaping()
+        .create();
+
+    return gson.toJson(list);
   }
 }
