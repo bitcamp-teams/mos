567249134205c3f9761c76af490152036e4d92ba
csw <susu199037@gmail.com>
2024-04-18 16:16:25 +0900

    Refactor: 로그인 모달 호출 수정

--- mos/src/main/resources/static/js/auth/modal.js
+++ mos/src/main/resources/static/js/auth/modal.js
@@ -1,28 +1,24 @@
 // auth/login.html 모달 구현
-document.addEventListener("DOMContentLoaded", () => {
-    const modal = document.getElementById('loginModal');
-    const btnOpenModal = document.querySelector('.btn-open-modal');
-    const loginModalContent = document.getElementById('loginModalContent');
+import auth from "../api/auth/auth.js";
 
-    // 로그인 버튼 클릭 시 모달 열기
-    btnOpenModal.addEventListener("click", async () => {
-      try {
-        // login.html 가져오기
-        const response = await fetch('/auth/login');
-        const htmlContent = await response.text();
-        // 모달 내용에 login.html 삽입
-        loginModalContent.innerHTML = htmlContent;
-        // 모달 열기
-        modal.style.display = "flex";
+const index = {
+    init() {
+        const _this = this;
 
-        // 모달 외부 클릭 시 모달 닫기
-      window.addEventListener("click", (event) => {
-        if (event.target === modal) {
-          modal.style.display = "none";
-        }
-      });
-      } catch (error) {
-        console.error("페이지를 가져오는 중 에러 발생:", error);
-      }
-    });
-  });
+        $(document).on("click", ".btn-open-modal", async () => {
+            _this.modalOpen();
+        })
+    },
+    modalOpen() {
+        auth.modalHtml().then(async res => {
+            const htmlContent = await res.data.body.innerHTML;
+            $('#loginModalContent').html(htmlContent);
+        })
+    },
+
+}
+
+index.init();
+
+
+
