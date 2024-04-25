3743ecef68217810d14cd87228bca5e5bb266fdf
sunjong0214 <rlatjswhd8200@gmail.com>
2024-04-25 12:25:42 +0900

    ãnotification

--- mos/src/main/java/com/mos/domain/noti/controller/NotiController.java
+++ mos/src/main/java/com/mos/domain/noti/controller/NotiController.java
@@ -24,8 +24,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class NotiController {
 
