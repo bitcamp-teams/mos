9b60c42c8e7b4513316037c1a258e05110dcfc52
sunjong0214 <rlatjswhd8200@gmail.com>
2024-04-25 18:06:15 +0900

    알림 리소스명 변경

--- mos/src/main/java/com/mos/domain/noti/dto/NotiUpdateDto.java
+++
@@ -1,9 +0,0 @@
-package com.mos.domain.noti.dto;
-
-import lombok.Data;
-
-@Data
-public class NotiUpdateDto {
-
-  private int id;
-}
--- mos/src/main/java/com/mos/domain/notify/dto/NotifyListDto.java
+++ mos/src/main/java/com/mos/domain/notify/dto/NotifyListDto.java
@@ -1,13 +1,11 @@
-package com.mos.domain.noti.dto;
+package com.mos.domain.notify.dto;
 
 import lombok.Data;
 
 @Data
-public class NotiDto {
-
+public class NotifyListDto {
   private int id;
   private int recipientId;
   private String message;
   private String link;
-  private boolean read;
 }
