dbb6c867f991f95e0fb1cf50bd25f054a19fca78
Joonhoe Kim <26rote@gmail.com>
2024-05-06 16:41:00 +0900

    Feat(Wiki): ToC !! 레이아웃 정리 필요 !!

--- mos/src/main/resources/static/js/wiki/wikitree-edit.js
+++ mos/src/main/resources/static/js/wiki/wikitree-edit.js
@@ -88,6 +88,24 @@ $(function () {
       history.pushState(null, null,
           '/wiki/view?studyNo=' + nodeContent.studyNo + '&wikiNo='
           + nodeContent.wikiNo);
+
+      return viewer;
+    })
+    .then(function (viewer) {
+      //프로미스가 이행되었다면 ToC를 생성한다.
+      let tocTarget = $('.toastui-editor-contents');
+      tocTarget.attr("data-spy", "scroll")
+      tocTarget.attr("data-target", "#toc")
+      console.log(tocTarget);
+
+      var navSelector = "#toc";
+      var $myNav = $(navSelector);
+      $myNav.html("");
+      Toc.init($myNav);
+      $(tocTarget[0]).scrollspy({
+        target: navSelector,
+      });
+
     })
   })
   .on('ready.jstree', function (e, data) {
@@ -232,4 +250,4 @@ function deleteSingleNode(data) {
 
 $('#addRootNode').on('click', function (e) {
   tree.jstree("create_node", '#');
-})
+});
