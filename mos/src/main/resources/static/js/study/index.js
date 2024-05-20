3c5766dcfb1031231a46f62d5c7e21f92db460d4
Joonhoe Kim <26rote@gmail.com>
2024-05-20 16:48:38 +0900

    Feat(wiki): 모두의 위키 카드UI => 첫번째 이미지를 썸네일에서 보이도록 변경, 이미지 없는 경우 랜덤하게 보이도록 변경(단순히 요청 URL을 쪼갠 것)

--- mos/src/main/resources/static/js/study/index.js
+++ mos/src/main/resources/static/js/study/index.js
@@ -1,127 +1,143 @@
 import study from "../api/study/study.js";
 import {formatDate} from "../util/util.js";
 
+let base = 167;
+let circle = 10;
+let count = 0;
+
 const index = {
-    currentPage: 0,
-    totalPages: 0,
-    flag: 'hit_count',
-    totalCount: 0,
-    searchText: '',
-    init() {
-        const _this = this;
-        $('.HomeTab_tab > a').on('click', function (e) {
-            e.preventDefault();
-            $('.loader').show();
-            $('.HomeTab_tab > a').removeClass('HomeTab_active');
-            $(this).addClass('HomeTab_active');
-            const flag = $(this).attr('data-flag');
-            if (flag === 'trending') {
-                _this.flag = 'hit_count';
-                $('.HomeTab_indicator').css('left', '4%');
-            } else if (flag === 'recent') {
-                _this.flag = 'created_date';
-                $('.HomeTab_indicator').css('left', '63.33%');
-            }
-            _this.initLoad(_this.flag);
-        })
+  currentPage: 0,
+  totalPages: 0,
+  flag: 'hit_count',
+  totalCount: 0,
+  searchText: '',
+  init() {
+    const _this = this;
+    $('.HomeTab_tab > a').on('click', function (e) {
+      e.preventDefault();
+      $('.loader').show();
+      $('.HomeTab_tab > a').removeClass('HomeTab_active');
+      $(this).addClass('HomeTab_active');
+      const flag = $(this).attr('data-flag');
+      if (flag === 'trending') {
+        _this.flag = 'hit_count';
+        $('.HomeTab_indicator').css('left', '4%');
+      } else if (flag === 'recent') {
+        _this.flag = 'created_date';
+        $('.HomeTab_indicator').css('left', '63.33%');
+      }
+      _this.initLoad(_this.flag);
+    })
 
-        document.addEventListener('DOMContentLoaded', function () {
-            _this.initLoad();
-            $('.search_searchInput').on('keypress', function (e) {
-                $('.search_searchInitialize').show();
-                if ($(this).val() === '') {
-                    $('.search_searchInitialize').hide();
-                }
-                // 엔터 치면 검색
-                if (e.keyCode === 13) {
-                    _this.searchText = $(this).val();
-                    _this.loadStudy(0, false, _this.flag);
-                }
-            });
-            $('.search_searchInitialize').on('click', function () {
-                $('.search_searchInput').val('');
-                $('.search_searchInitialize').hide();
-                _this.searchText = '';
-            });
-
-        });
-
-        $(window).scroll(function () {
-            //전체 문서의 높이
-            const documentHeight = document.body.offsetHeight;
-            //(현재 화면상단 + 현재 화면 높이)
-            const nowHeight = window.scrollY + window.innerHeight;
-            if (nowHeight >= documentHeight) {
-                console.log(_this.currentPage,  _this.totalPages)
-                if (_this.currentPage < _this.totalPages) {
-                    _this.loadStudy(_this.currentPage, true);
-                }
-            }
-        });
-
-    },
-    initLoad(flag = 'hit_count') {
-        const _this = this;
-        // 초기 페이지 번호 설정
-        _this.currentPage = 0;
-        _this.totalPages = 0;
-        $(document).scrollTop(0);
-
-        _this.loadStudy(_this.currentPage,false, flag);
-    },
-    loadStudy(page, append = false, flag = 'hit_count') {
-        const _this = this;
-        const cardsContainer = $('.PostCardGrid_block');
-
-        const params = {
-            page: page,
-            flag: flag,
-            searchText: _this.searchText
-        };
-
-        $('.loader').show();
-        if (!append) {
-            cardsContainer.html('');
+    document.addEventListener('DOMContentLoaded', function () {
+      _this.initLoad();
+      $('.search_searchInput').on('keypress', function (e) {
+        $('.search_searchInitialize').show();
+        if ($(this).val() === '') {
+          $('.search_searchInitialize').hide();
         }
+        // 엔터 치면 검색
+        if (e.keyCode === 13) {
+          _this.searchText = $(this).val();
+          _this.loadStudy(0, false, _this.flag);
+        }
+      });
+      $('.search_searchInitialize').on('click', function () {
+        $('.search_searchInput').val('');
+        $('.search_searchInitialize').hide();
+        _this.searchText = '';
+      });
 
-        study.findAll(params).then(res => {
-            if (res.data == null) {
-                throw new Error('스터디 정보를 불러올 수 없음');
-            }
-            $('#totalCount').text(res.data.totalElements);
-            _this.totalPages = res.data.totalPages;
-            _this.currentPage++;
-            return res.data.content;
-        }).then(studyList => {
-            if (!studyList || studyList.length === 0) {
-                cardsContainer.html('데이터가 없습니다.');
-            } else {
-                cardsContainer.append(_this.createCards(studyList));
-            }
-            $('.loader').delay('500').fadeOut();
-        });
-    },
-    createCards(studyList) {
-        const cardsArr = [];
-        studyList.forEach(function (study) {
-            const li = $('<li class="PostCard_block"></li>');
-            // 썸네일 영역
-            const thumbnail = $(`<a href="/study/view?studyNo=${study.studyNo}" class="VLink_block PostCard_styleLink">
+    });
+
+    $(window).scroll(function () {
+      //전체 문서의 높이
+      const documentHeight = document.body.offsetHeight;
+      //(현재 화면상단 + 현재 화면 높이)
+      const nowHeight = window.scrollY + window.innerHeight;
+      if (nowHeight >= documentHeight) {
+        console.log(_this.currentPage, _this.totalPages)
+        if (_this.currentPage < _this.totalPages) {
+          _this.loadStudy(_this.currentPage, true);
+        }
+      }
+    });
+
+  },
+  initLoad(flag = 'hit_count') {
+    const _this = this;
+    // 초기 페이지 번호 설정
+    _this.currentPage = 0;
+    _this.totalPages = 0;
+    $(document).scrollTop(0);
+
+    _this.loadStudy(_this.currentPage, false, flag);
+  },
+  loadStudy(page, append = false, flag = 'hit_count') {
+    const _this = this;
+    const cardsContainer = $('.PostCardGrid_block');
+
+    const params = {
+      page: page,
+      flag: flag,
+      searchText: _this.searchText
+    };
+
+    $('.loader').show();
+    if (!append) {
+      cardsContainer.html('');
+    }
+
+    study.findAll(params).then(res => {
+      if (res.data == null) {
+        throw new Error('스터디 정보를 불러올 수 없음');
+      }
+      $('#totalCount').text(res.data.totalElements);
+      _this.totalPages = res.data.totalPages;
+      _this.currentPage++;
+      return res.data.content;
+    }).then(studyList => {
+      if (!studyList || studyList.length === 0) {
+        cardsContainer.append('<div>데이터가 없습니다.</div>');
+      } else {
+        cardsContainer.append(_this.createCards(studyList));
+      }
+      $('.loader').delay('500').fadeOut();
+    });
+  },
+  createCards(studyList) {
+    const cardsArr = [];
+    studyList.forEach(function (study) {
+
+      //썸네일 교체. 우선순위: UUID/1번이미지/랜덤이미지
+      let thumbnailUrl = 'https://picsum.photos/320/' + (base + count++
+          % circle);
+      if (study.thumbnail != null) {
+        thumbnailUrl = study.thumbnail;
+      } else if (extractImageUrl(study.content) != null) {
+        thumbnailUrl = extractImageUrl(study.content);
+      }
+
+      // 썸네일 영역
+      const li = $('<li class="PostCard_block"></li>');
+      const thumbnail = $(`<a href="/study/view?studyNo=${study.studyNo}" class="VLink_block PostCard_styleLink">
                                             <div class="RatioImage_block" style="padding-top: 52.1921%;">
                                                 <img alt="thumbnail"
                                                      fetchpriority="high"
                                                      decoding="async"
                                                      data-nimg="fill"
-                                                     src="https://picsum.photos/320/167"
+                                                     src="${thumbnailUrl}"
                                                      style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;">
                                             </div>
                                         </a>`);
 
-            // 본문
-            const date = formatDate(study.createdDate);
-            // photo 가 없으면 ''
-            const photo = (study.photo != null) ? `https://4l8fsxs62676.edge.naverncp.com/iBroLT7rzG/member/${study.photo}?type=f&w=32&h=32&ttype=jpg` : '/img/icon2.png';
-            const content = $(`<div class="PostCard_content">
+      // 본문
+      const date = formatDate(study.createdDate);
+      // photo 가 없으면 ''
+      const photo = (study.photo != null)
+          ? `https://4l8fsxs62676.edge.naverncp.com/iBroLT7rzG/member/${study.photo}?type=f&w=32&h=32&ttype=jpg`
+          : '/img/icon2.png';
+      const content = $(`<div class="PostCard_content">
                                             <a href="/study/view?studyNo=${study.studyNo}"
                                                class="VLink_block PostCard_styleLink">
                                                 <h4 class="PostCard_h4 utils_ellipsis">${study.title}</h4>
@@ -133,8 +149,9 @@ const index = {
                                                 <span class="PostCard_separator">·</span><span>${study.commentCount}개의 댓글</span>
                                             </div>
                                         </div>`);
-            // 유저 정보
-            const userInfo = $(`<div class="PostCard_footer"><a class="PostCard_userInfo" href="/member/dashboard?no=${study.memberNo}">
+      // 유저 정보
+      const userInfo = $(
+          `<div class="PostCard_footer"><a class="PostCard_userInfo" href="/member/dashboard?no=${study.memberNo}">
                                             <img
                                                     alt="user thumbnail of heyday.xz" loading="lazy" width="24" height="24"
                                                     decoding="async"
@@ -148,14 +165,14 @@ const index = {
                                                 ${study.likeCount}
                                             </div>
                                         </div>`)
-            li.append(thumbnail);
-            li.append(content);
-            li.append(userInfo);
+      li.append(thumbnail);
+      li.append(content);
+      li.append(userInfo);
 
-            cardsArr.push(li)
-        });
-        return cardsArr;
-    }
+      cardsArr.push(li)
+    });
+    return cardsArr;
+  }
 };
 
 index.init();
--- mos/src/main/resources/static/js/wiki/index.js
+++ mos/src/main/resources/static/js/wiki/index.js
@@ -79,7 +79,7 @@ const index = {
       return res.data.content;
     }).then(wikiList => {
       if (wikiList.length === 0) {
-        cardsContainer.html('데이터가 없습니다.');
+        cardsContainer.append('<div>데이터가 없습니다.</div>');
       } else {
         cardsContainer.append(_this.createCards(wikiList));
       }
@@ -89,8 +89,8 @@ const index = {
   createCards(wikiList) {
     const cardsArr = [];
     wikiList.forEach(function (wiki) {
-      //썸네일 교체. 우선순위: UUID/1번이미지/랜덤이미지
 
+      //썸네일 교체. 우선순위: UUID/1번이미지/랜덤이미지
       let thumbnailUrl = 'https://picsum.photos/320/' + (base + count++
           % circle);
       if (wiki.thumbnail != null) {
@@ -98,8 +98,9 @@ const index = {
       } else if (extractImageUrl(wiki.content) != null) {
         thumbnailUrl = extractImageUrl(wiki.content);
       }
-      const li = $('<li class="PostCard_block"></li>');
+
       // 썸네일 영역
+      const li = $('<li class="PostCard_block"></li>');
       const thumbnail = $(
           `<a href="/wiki/view?studyNo=${wiki.studyNo}&wikiNo=${wiki.wikiNo}" class="VLink_block PostCard_styleLink">
                                             <div class="RatioImage_block" style="padding-top: 52.1921%;">
@@ -155,57 +156,5 @@ const index = {
     return cardsArr;
   }
 };
-
-function extractImageUrl(text) {
-  // text가 null이나 undefined인 경우 null 반환
-  if (!text) {
-    return null;
-  }
-
-  const regex = /!\[(.*?)]\((.*?)\)/;
-  const match = text.match(regex);
-
-  if (match && match.length > 2 && isValidImageUrl(match[2])) {
-    return match[2];
-  }
-
-  return null;
-}
-
-function isValidImageUrl(url) {
-  return new Promise((resolve) => {
-    // URL이 비어있거나 null인 경우 false 반환
-    if (!url) {
-      resolve(false);
-      return;
-    }
-
-    try {
-      // URL을 생성하고, 프로토콜이 http나 https인지 확인
-      const parsedUrl = new URL(url);
-      if (!['http:', 'https:'].includes(parsedUrl.protocol)) {
-        resolve(false);
-        return;
-      }
-
-      // 이미지 확장자 검사
-      const validExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp'];
-      const extension = parsedUrl.pathname.split('.').pop().toLowerCase();
-      if (!validExtensions.includes(extension)) {
-        resolve(false);
-        return;
-      }
-
-      // Image 객체를 사용하여 이미지 로드 및 렌더링 가능 여부 확인
-      const img = new Image();
-      img.onload = () => resolve(true);
-      img.onerror = () => resolve(false);
-      img.src = url;
-    } catch (error) {
-      // URL 형식이 잘못된 경우 false 반환
-      resolve(false);
-    }
-  });
-}
 
 index.init();
